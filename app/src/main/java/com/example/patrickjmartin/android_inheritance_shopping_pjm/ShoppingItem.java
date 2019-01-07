84b55fea303fb17aa147b2ff730e58e62f85cbb4
Patrick Martin <patrickmartin88@gmail.com>
2019-01-07 12:19:14 -0800

    Completed ShoppingItem class and initial creation of android environment.

---
+++ app/src/main/java/com/example/patrickjmartin/android_inheritance_shopping_pjm/ShoppingItem.java
@@ -0,0 +1,16 @@
+package com.example.patrickjmartin.android_inheritance_shopping_pjm;
+
+public class ShoppingItem {
+
+    private int colorID;
+    private String productName;
+
+    public ShoppingItem(int colorID, String productName) {
+        this.colorID = colorID;
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+}

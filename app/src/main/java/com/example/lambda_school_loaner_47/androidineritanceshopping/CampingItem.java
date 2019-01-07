78bf560d0f3a3d81810234ef0419907a3b3679c8
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-07 17:43:36 -0600

    finish classes

--- app/src/main/java/com/example/lambda_school_loaner_47/androidineritanceshopping/CampingItem.java
+++ app/src/main/java/com/example/lambda_school_loaner_47/androidineritanceshopping/CampingItem.java
@@ -1,10 +1,19 @@
 package com.example.lambda_school_loaner_47.androidineritanceshopping;
 
+import android.graphics.Color;
+
 public class CampingItem extends ShoppingItem {
-    protected int colorId;
+    private static int COLOR_ID = Color.WHITE;
     protected String hunting;
 
     protected CampingItem(int colorId, String productName) {
         super(colorId, productName);
+        this.hunting = "Its sharp";
+    }
+
+    @Override
+    public String getDisplayName() {
+        super.getDisplayName();
+        return hunting;
     }
 }
--- app/src/main/java/com/example/lambda_school_loaner_47/androidineritanceshopping/ElectronicItem.java
+++ app/src/main/java/com/example/lambda_school_loaner_47/androidineritanceshopping/ElectronicItem.java
@@ -1,10 +1,19 @@
 package com.example.lambda_school_loaner_47.androidineritanceshopping;
 
+import android.graphics.Color;
+
 public class ElectronicItem extends ShoppingItem {
-    protected int colorId;
+    private static int COLOR_ID = Color.RED;
     protected String batteries;
 
+    @Override
+    public String getDisplayName() {
+        super.getDisplayName();
+        return batteries;
+    }
+
     protected ElectronicItem(int colorId, String productName) {
-        super(colorId, productName);
+        super(COLOR_ID, productName);
+        this.batteries = "Make sure they're charged";
     }
 }

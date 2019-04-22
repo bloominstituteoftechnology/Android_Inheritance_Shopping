d0bf08c124a3789db16d7ebc1a6c3b1c7e69ace3
Jake Esveld <thejakeesveld@gmail.com>
2019-04-22 12:27:37 -0700

    Add recyclerView and listAdapter. Bolt to main Activity

--- app/src/main/java/com/jakeesveld/android_inheritance_shopping/MainActivity.java
+++ app/src/main/java/com/jakeesveld/android_inheritance_shopping/MainActivity.java
@@ -2,12 +2,25 @@ package com.jakeesveld.android_inheritance_shopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ShoppingListAdapter listAdapter;
+    ArrayList<ShoppingItem> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        itemList = new ArrayList<>();
+        listAdapter = new ShoppingListAdapter(itemList);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+
+
     }
 }
---
+++ app/src/main/java/com/jakeesveld/android_inheritance_shopping/ShoppingListAdapter.java
@@ -0,0 +1,50 @@
+package com.jakeesveld.android_inheritance_shopping;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ShoppingListAdapter extends RecyclerView.Adapter<ShoppingListAdapter.ViewHolder> {
+
+    ArrayList<ShoppingItem> dataList;
+
+    public ShoppingListAdapter(ArrayList<ShoppingItem> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.shopping_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        ShoppingItem data = dataList.get(i);
+        viewHolder.textTitle.setText(data.getName());
+        viewHolder.parentView.setBackgroundColor(data.getColorId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        View parentView;
+        TextView textTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.text_title);
+            parentView = itemView.findViewById(R.id.layout_parent);
+        }
+    }
+}
